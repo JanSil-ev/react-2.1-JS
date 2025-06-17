e4d87be5dc40382235aa86737919f1c4acbc53cd
Silantiev Jan <Silantiev-999@mail.ru>
2025-06-17 22:04:53 +0300

    relis

---
+++ src/components/CarTableRow.jsx
@@ -0,0 +1,28 @@
+export const CarTableRow = ({ car }) => (
+  <tr key={car.brand + car.model}>
+    <td>
+      {car.brand} {car.model}
+    </td>
+    <td>
+      {car.price} {car.сurrency}
+    </td>
+    <td>{car.year}</td>
+    <td>
+      <select>
+        {car.color.map((color, index) => (
+          <option key={index} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
+    </td>
+    <td>
+      <button
+        className={car.isReserved ? "reserved" : ""}
+        disabled={car.isReserved}
+      >
+        {car.isReserved ? "Забронировано" : "Забронировать"}
+      </button>
+    </td>
+  </tr>
+);
